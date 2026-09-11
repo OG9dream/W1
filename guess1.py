1cde090a958ae8ae9347d8a825c1f7dbd4dbeeda
OG9dream <3178956441@qq.com>
2026-09-11 10:56:00 +0800

    修正拼写错误

--- guess1.py
+++ guess1.py
@@ -3,9 +3,9 @@ goal=random.randint(1, 100)
 num=int(input("请输入一个数:"))
 while goal!=num:
     if goal<num:
-        print("be smaller")
+        print("猜小点")
         num=int(input("请重新输入一个数:"))
     else:
-        print("be bigger")
+        print("猜大点")
         num=int(input("请重新输入一个数:"))
-print("congratrulation")
+print("congratulation")
